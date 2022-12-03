21b2e7afe9e9773cb6b364ff56adfb0245952e8a
kttykat <kat@saige.lgbt>
2022-12-03 17:42:22 +0100

    inital Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx(), tailwind()],
-	server: {
-		port: 7233,
-	},
 	markdown: {
 		shikiConfig: {
 			theme: "one-dark-pro",
